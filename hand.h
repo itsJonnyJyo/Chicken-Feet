e6f5e4f081af0a5c7ed46c746f76b1f33c3511c5
Scott <scott.e.pickthorn@gmail.com>
2015-01-15 15:37:34 -0800

    add this file

---
+++ hand.h
@@ -0,0 +1,21 @@
+#ifndef HAND_H
+#define HAND_H
+
+#include "card.h"
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+class Hand{
+public:
+	Hand();
+	~Hand();
+
+	void addBone(Bones *aBone);
+	bool removeBone(int pos, Bone*& aBone);
+	void print() const;
+private:
+	vector<Bone*> bones;
+};
+#endif
