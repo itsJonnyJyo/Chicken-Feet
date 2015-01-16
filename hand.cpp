235a242aac4a62eb179ccd35345a63e22bb47c7b
itsJonnyJyo <itsJonnyJyo@users.noreply.github.com>
2015-01-15 16:54:24 -0800

    Update hand.cpp

--- hand.cpp
+++ hand.cpp
@@ -3,25 +3,25 @@
 
 Hand::Hand(){}
 Hand:: ~Hand(){
-	for(auto it = bones.begin(); it != bones.end(); it++){
+	for(auto it = hand.begin(); it != hand.end(); it++){
 		delete *it;
 	}
 }
 
 void Hand::addBone(Bone * bone){
-	bones.push_back(bone);
+	hand.push_back(bone);
 }
 
 bool Hand::removeBone(int pos, Bone*& abone){
-	if(pos < || pos >= bone.size())
+	if(pos < || pos >= hand.size())
 		return false;
-	abone = bones.at(pos);
-	bones.erase(bones.begin + pos);
+	abone = hand.at(pos);
+	hand.erase(hand.begin + pos);
 	return true;
 
 }
 Void print() const {
-	for(auto it = bones.cbegin(); it != bones.cend(); it++){
+	for(auto it = hand.cbegin(); it != hand.cend(); it++){
 		(*it)->print();
 		cout << " ";
 	}
